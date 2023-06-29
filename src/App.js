6deab9c84cb02f73f74ff434e8fbeec88beff080
CertifySphere <132380723+certifysphere@users.noreply.github.com>
2023-06-29 12:17:37 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import MainSection from './components/main-components';
+
+
 
 function App() {
   return (
-   <HomeComp/>
+    <>
+    <main>
+      <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">Album example</h1>
+            <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+            <p>
+              <a href="#" class="btn btn-primary my-2" >Main call to action</a>
+              <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+
+      <div class="album py-5 bg-body-tertiary">
+        <div class="container">
+
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+              <div class="card shadow-sm">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-body-secondary">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
-function HomeComp() {
-  return (
-    <div className="App">
-    <body>
-    <p>
-        Welcome to PublicToilet Listing APP!!!
-    </p>  
-    <AddToilet/>
-    <ListToilets/>
-    </body>  
-    </div>
-  );
-}
-
-function AddToilet() {
-  return (<button>Click Here Add A New Toilet.</button>);
-}
-
-function ListToilets(){
-  return (
-  <>
-   <h1>City Public-Toilet-Information</h1>
-  <hr></hr>
-  <label for="citySelect">Select City:</label>
-  <select id="citySelect" onchange="displayCityInfo()">
-    <option value="Moradabad">Moradabad</option>
-    <option value="Mumbai">Mumbai</option>
-    <option value="Kanpur">Kanpur</option>
-    <option value="Goa">Goa</option>
-    <option value="Delhi">Delhi</option>
-  </select>
-  <hr></hr>
-  <table>
-    <thead>
-      <tr>
-        <th>City</th>
-        <th>Toilet-Locations</th>
-        <th>Available</th>
-      </tr>
-    </thead>
-    <tbody id="cityInfoTableBody"></tbody>
-  </table>
-  </>);
-}
 
 export default App;
